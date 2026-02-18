594bf3b22482bd92566d1670d3ed8fc46a0cc9b8
atif-nabeel <atifnabeel.working@gmail.com>
2026-02-18 22:45:01 +0500

    man

--- fristPython.py
+++ fristPython.py
@@ -2,8 +2,7 @@ name = "Muhammad Atif Nabeel"
 rollnumb = 77
 age = 19
 marks = 97.9
-print(type(name))
-print(type(rollnumb))
-print(type(age))
-print(type(marks))
+age2 = age
+print (age2)
+print (age)
 
