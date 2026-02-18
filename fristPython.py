4aa29d3943ebfecae5b699c3f0b2507e6ba0e988
atif-nabeel <atifnabeel.working@gmail.com>
2026-02-18 22:57:03 +0500

    student records

--- fristPython.py
+++ fristPython.py
@@ -5,11 +5,11 @@ marks = 97.9
 age2 = age
 name2 = name
 rollnumb2 = 15
-print(name)
+print("student 1 name is :",name)
 print("student rollnumber is :",rollnumb)
 print ("student age is :",age)
-print(name2)
-print(rollnumb2)
-print (age2)
+print("student 2 name is :",name2)
+print("student 2 rollnumb is :",rollnumb2)
+print ("student 2 age is:",age2)
 
 
