7c0fd8b56b5d96043c4c623b5ab67a997621822f
atif-nabeel <atifnabeel.working@gmail.com>
2026-02-19 21:49:00 +0500

    man

--- fristPython.py
+++ fristPython.py
@@ -1,11 +1,13 @@
 name = "Muhammad Atif Nabeel"
 rollnumb = 77
+study = "BSC"
 age = 19
 marks = 97.9
 age2 = age
 name2 = name
 rollnumb2 = 15
 print("student 1 name is :",name)
+print(study)
 print("student rollnumber is :",rollnumb)
 print ("student age is :",age)
 print("student 2 name is :",name2)
@@ -13,5 +15,6 @@ print("student 2 rollnumb is :",rollnumb2)
 print ("student 2 age is:",age2)
 print ("best of luck",name)
 print(35+45)
+print()
 
 
