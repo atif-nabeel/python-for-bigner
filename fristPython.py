06388f1daca1d2f41981b807cf4b20b666e2a25b
atif-nabeel <atifnabeel.working@gmail.com>
2026-02-20 13:05:54 +0500

    AND operator

--- fristPython.py
+++ fristPython.py
@@ -13,8 +13,16 @@
 print("***logical operator***")
 a = 50
 b = 30
-print ( not(a>b))
-print (a<b)
+val1 , val2= True,True
+print("1 AND operator:", val1 and val2)
+val3 , val4 = False, True
+print("2 AND operator is :", val3 and val4)
+val5 , val6 = False , False
+print("3 AND operator is :",val5 and val6)
+
+
+# print ( not(a>b))
+# print (a<b)
 
 
 
