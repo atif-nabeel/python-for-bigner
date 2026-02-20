6cf094c4c84b048f8c47ae3962f6d81b1ffb3729
atif-nabeel <atifnabeel.working@gmail.com>
2026-02-20 11:37:53 +0500

    logical operators

--- fristPython.py
+++ fristPython.py
@@ -1,15 +1,20 @@
 # relational"
 
-num = 10
+# num = 10
 
-num **= 5
+# num **= 5
 # print(a == b)
 # print(a != b)
 # print(a >= b)
 # print(a > b)
 # print(a <= b)
-# print(a < b)
-print ("num :", num)
+#  print(a < b)
+# print ("num :", num)
+print("***logical operator***")
+a = 50
+b = 30
+print (not False)
+print (not True)
 
 
 
