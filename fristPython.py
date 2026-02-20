edec88621968a4ec8333108cf27ea17c97fe134d
atif-nabeel <atifnabeel.working@gmail.com>
2026-02-20 11:20:01 +0500

    += operations

--- fristPython.py
+++ fristPython.py
@@ -1,13 +1,15 @@
 # relational"
 
-a = 10
-b = 10
-print(a == b)
-print(a != b)
-print(a >= b)
-print(a > b)
-print(a <= b)
-print(a < b)
+num = 10
+
+num += 10
+# print(a == b)
+# print(a != b)
+# print(a >= b)
+# print(a > b)
+# print(a <= b)
+# print(a < b)
+print ("num :", num)
 
 
 
