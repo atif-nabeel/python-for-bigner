50af54e22afaa455d907862fe2db8d0a28098fef
atif-nabeel <atifnabeel.working@gmail.com>
2026-02-20 13:14:33 +0500

    OR

--- fristPython.py
+++ fristPython.py
@@ -14,11 +14,11 @@ print("***logical operator***")
 a = 50
 b = 30
 val1 , val2= True,True
-print("1 AND operator:", val1 and val2)
+print("1 OR operator:", val1 or val2)
 val3 , val4 = False, True
-print("2 AND operator is :", val3 and val4)
+print("2 OR operator is :", val3 or  val4)
 val5 , val6 = False , False
-print("3 AND operator is :",val5 and val6)
+print("3 OR operator is :",val5 or  val6)
 
 
 # print ( not(a>b))
