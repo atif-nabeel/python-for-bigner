14ac9f277d444a7e349a09886e7786510619f999
atif-nabeel <atifnabeel.working@gmail.com>
2026-02-20 14:05:16 +0500

    type conversion

--- fristPython.py
+++ fristPython.py
@@ -10,19 +10,24 @@
 # print(a <= b)
 #  print(a < b)
 # print ("num :", num)
-print("***logical operator***")
-a = 50
-b = 30
-val1 , val2= True,True
-print("1 OR operator:", val1 and val2)
-val3 , val4 = False, True
-print("2 OR operator is :", (a == b) or  (a > b))
-val5 , val6 = False , False
-print("3 OR operator is :",(a < b) or  (a < b))
+# print("***logical operator***")
+# a = 50
+# b = 30
+# val1 , val2= True,True
+# print("1 OR operator:", val1 and val2)
+# val3 , val4 = False, True
+# print("2 OR operator is :", (a == b) or  (a > b))
+# val5 , val6 = False , False
+# print("3 OR operator is :",(a < b) or  (a < b))
 
 
 # print ( not(a>b))
 # print (a<b)
+# Type conversion
+a = 2
+b = 4.34 
+sum = a + b
+print (sum)
 
 
 
