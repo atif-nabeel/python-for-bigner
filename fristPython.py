7c93e566c7062f309bfcc9cf93b03bd6411f6ab6
atif-nabeel <atifnabeel.working@gmail.com>
2026-02-20 10:49:32 +0500

    arithematics

--- fristPython.py
+++ fristPython.py
@@ -1,31 +1,11 @@
-name = "Muhammad Atif Nabeel"
-rollnumb = 77
-study = "BSC"
-study2 = "BSC"
-age = 19
-matric = 805
-matric2 = 925
-
-age2 = age
-name2 =  "Atif"
-rollnumb2 = 15
-print("*** student Records ***")
-print("student 1 name is :",name)
-print("atudent 1 matric marks is:",matric)
-print(" Student 1 study program is :",study)
-print("student  1 rollnumber is :",rollnumb)
-print ("student 1  age is :",age)
-print("student 2 name is :",name2)
-
-print("matric marks is :",matric2)
-print("student 2 rollnumb is :",rollnumb2)
-print ("student 2 age is:",age2)
-print ("best of luck",name)
-print("*** some Math operation***")
-
-a = 50
+# arithematics operators
+a = 29
 b = 77
-sum = a + b
-print("sum of 2 digits:",sum)
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+
 
 
