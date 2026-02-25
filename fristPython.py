cdc0a0af529efab9f6a86ffe59f25cedc83c1671
atif-nabeel <atifnabeel.working@gmail.com>
2026-02-25 16:44:06 +0500

    frist digit find

--- fristPython.py
+++ fristPython.py
@@ -75,11 +75,11 @@ str = "my name is atif nabeel"
 
 new_str = str.replace("my name is atif nabeel"," my name is muhammad faizan and i am a good student")
 print(new_str)
-print(new_str.find("muhammad faizan"))
+print(new_str.find("my"))
 
 
 print(str)
-print(str.find("f"))
+print(str.find("my"))
 
 
 
