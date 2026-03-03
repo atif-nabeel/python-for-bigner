a31d9592a50e9ec8c050400597e07ba91d82745c
atif-nabeel <atifnabeel.working@gmail.com>
2026-03-03 13:49:39 +0500

    student list

--- lecture 3.py
+++ lecture 3.py
@@ -1,6 +1,8 @@
-marks = [66.7,88.9,56.9,93.5]
-print(marks)
-print(type(marks))
-print(marks[0])
-print (marks[1])
-print(len(marks))
+# marks = [66.7,88.9,56.9,93.5]
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print (marks[1])
+# print(len(marks))
+student = ["atif", 93, 20,"pindi"]
+print(student)
