06a0c1ad6c3715db70db24654249d118a6b8e553
atif-nabeel <atifnabeel.working@gmail.com>
2026-03-05 15:03:29 +0500

    add new key

--- lecture4.py
+++ lecture4.py
@@ -1,14 +1,18 @@
 print("***** Dictionaries ******")
 student = {
   "Name" : "Atif",
+  "NabeelName" : " Faizan",
+
   "CGPA"  : 3.8,
   "age"  :  20,
   "marks" : [ 98,97,96,95],
   "subjects" : [ "python","c","javascript","css"],
 }
-print(dict)
+print(student)
 student["Name"] = "Nabeel"
+student["NabeelName "] = " Muhammad Faizan"
 print(student["Name"])
+print(student["NabeelName"])
 print(student["age"])
 print(student["CGPA"])
 print(student["marks"])
