c071e46787cc498fea9ed5ffcd7974db466901d9
atif-nabeel <atifnabeel.working@gmail.com>
2026-03-05 15:19:03 +0500

    add value

--- lecture4.py
+++ lecture4.py
@@ -1,21 +1,24 @@
-# print("***** Dictionaries ******")
-# student = {
-#   "Name" : "Atif",
-#   "CGPA"  : 3.8,
-#   "age"  :  20,
-#   "marks" : [ 98,97,96,95],
-#   "subjects" : [ "python","c","javascript","css"],
-# }
-# print(student)
-# student["Name"] = "Nabeel"
+print("***** Dictionaries ******")
+student = {
+  "Name" : "Atif",
+  "CGPA"  : 3.8,
+  "age"  :  20,
+  "marks" : [ 98,97,96,95],
+  "subjects" : [ "python","c","javascript","css"],
+}
+print(student)
+student["Name"] = "Nabeel"
 
-# print(student["Name"])
-# print(student["NabeelName"])
-# print(student["age"])
-# print(student["CGPA"])
-# print(student["marks"])
-# print(student["subjects"])
+print(student["Name"])
+
+print(student["age"])
+print(student["CGPA"])
+print(student["marks"])
+print(student["subjects"])
 null_dict = {}
 null_dict["name"] = " muhammad Faizan "
+null_dict["age"] = 20
+
+
 print(null_dict)
 
