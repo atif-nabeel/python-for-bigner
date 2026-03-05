af36b86b70d545e136c0752da2668e8192701378
atif-nabeel <atifnabeel.working@gmail.com>
2026-03-05 15:07:01 +0500

    null_dict mein value add

--- lecture4.py
+++ lecture4.py
@@ -1,20 +1,21 @@
-print("***** Dictionaries ******")
-student = {
-  "Name" : "Atif",
-  "NabeelName" : " Faizan",
+# print("***** Dictionaries ******")
+# student = {
+#   "Name" : "Atif",
+#   "CGPA"  : 3.8,
+#   "age"  :  20,
+#   "marks" : [ 98,97,96,95],
+#   "subjects" : [ "python","c","javascript","css"],
+# }
+# print(student)
+# student["Name"] = "Nabeel"
 
-  "CGPA"  : 3.8,
-  "age"  :  20,
-  "marks" : [ 98,97,96,95],
-  "subjects" : [ "python","c","javascript","css"],
-}
-print(student)
-student["Name"] = "Nabeel"
-student["NabeelName "] = " Muhammad Faizan"
-print(student["Name"])
-print(student["NabeelName"])
-print(student["age"])
-print(student["CGPA"])
-print(student["marks"])
-print(student["subjects"])
+# print(student["Name"])
+# print(student["NabeelName"])
+# print(student["age"])
+# print(student["CGPA"])
+# print(student["marks"])
+# print(student["subjects"])
+null_dict = {}
+null_dict["name"] = " muhammad Faizan "
+print(null_dict)
 
