c3f86da7b96c248baacd0cf1893dd60a55d34728
atif-nabeel <atifnabeel.working@gmail.com>
2026-03-05 15:27:09 +0500

    nested

--- lecture4.py
+++ lecture4.py
@@ -1,24 +1,37 @@
-print("***** Dictionaries ******")
-student = {
-  "Name" : "Atif",
-  "CGPA"  : 3.8,
-  "age"  :  20,
-  "marks" : [ 98,97,96,95],
-  "subjects" : [ "python","c","javascript","css"],
+# print("***** Dictionaries ******")
+# student = {
+#   "Name" : "Atif",
+#   "CGPA"  : 3.8,
+#   "age"  :  20,
+#   "marks" : [ 98,97,96,95],
+#   "subjects" : [ "python","c","javascript","css"],
+# }
+# print(student)
+# student["Name"] = "Nabeel"
+
+# print(student["Name"])
+
+# print(student["age"])
+# print(student["CGPA"])
+# print(student["marks"])
+# print(student["subjects"])
+# null_dict = {}
+# null_dict["name"] = " muhammad Faizan "
+# null_dict["age"] = 20
+
+
+
+
+# print(null_dict)
+Student ={
+  "name" : " Atif nabeel",
+  "age" : 20,
+  "scrore" : {
+    "comput" : 98,
+    "math" : 98,
+    "phy" : 97,
+  }
 }
-print(student)
-student["Name"] = "Nabeel"
+print(Student)
 
-print(student["Name"])
-
-print(student["age"])
-print(student["CGPA"])
-print(student["marks"])
-print(student["subjects"])
-null_dict = {}
-null_dict["name"] = " muhammad Faizan "
-null_dict["age"] = 20
-
-
-print(null_dict)
 
