a9dacfa9b82c177fe8568bb29cb4285565618aea
atif-nabeel <atifnabeel.working@gmail.com>
2026-03-06 22:12:55 +0500

    Atif while loop

--- lecture4.py
+++ lecture4.py
@@ -49,10 +49,10 @@
 # set2 = {1,5,7,8,10,12}
 # print(collection.union(set2))
 # print(collection.intersection(set2))
-count = 1 
-while count <=5 :
-  print("Hello")
-  count += 1
+i = 1 
+while i <= 10000:
+  print("Atif",i)
+  i +=1
 
 
 
