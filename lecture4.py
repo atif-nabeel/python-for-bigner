dada34676cd97d183b364ed186e79c83f96a8cce
atif-nabeel <atifnabeel.working@gmail.com>
2026-03-06 15:30:34 +0500

    empty set

--- lecture4.py
+++ lecture4.py
@@ -22,29 +22,30 @@
 
 
 
-# print(null_dict)
-Student ={
-  # new_dict = { "city" : "islamabad", " age " : 16}
-  "name" : " Atif nabeel",
-  "age" : 20,
-  "city" : " lahore"
-  "score" : {
-    "comput" : 98,
-    "math" : 98,
-    "phy" : 97,
-  }
-}
-print(Student.keys())
-print(list(Student.keys()))
-print(Student.values())
-print(list(Student.values()))
-print(list(Student.items()))
-print(Student.get("name"))
-print(Student.get("name2"))
-# Student.update({"name":"kashif"})
-new_dict = { "city" : "islamabad", " age " : 16}
-Student.update(new_dict)
-print(Student)
+# # print(null_dict)
+# Student ={
+#   # new_dict = { "city" : "islamabad", " age " : 16}
+#   "name" : " Atif nabeel",
+#   "age" : 20,
+#   "city" : " lahore"
+#   "score" : {
+#     "comput" : 98,
+#     "math" : 98,
+#     "phy" : 97,
+#   }
+# }
+# print(Student.keys())
+# print(list(Student.keys()))
+# print(Student.values())
+# print(list(Student.values()))
+# print(list(Student.items()))
+# print(Student.get("name"))
+# print(Student.get("name2"))
+# # Student.update({"name":"kashif"})
+# new_dict = { "city" : "islamabad", " age " : 16}
+# Student.update(new_dict)
+# print(Student)
+colllection = set()
 
 
 
