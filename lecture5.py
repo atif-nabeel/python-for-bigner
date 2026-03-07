05492d5fd23843943c48b59b13a288c43d9c16b4
atif-nabeel <atifnabeel.working@gmail.com>
2026-03-07 11:42:08 +0500

    19 table input

--- lecture5.py
+++ lecture5.py
@@ -1,6 +1,11 @@
-i = 1
-while i <=10:
-  print( 17*i)
-  i += 1
+# i = 1
+# while i <=10:
+#   print( 17*i)
+#   i += 1
 
-print (" loop ending")
+# print (" loop ending")
+n = int(input("enter number :"))
+i = 1
+while i <= 10:
+  print(n*i)
+  i +=1
