193184612fafd200162ac02dffac4305247259b1
atif-nabeel <atifnabeel.working@gmail.com>
2026-03-07 11:52:18 +0500

    student

--- lecture5.py
+++ lecture5.py
@@ -10,7 +10,8 @@
 #   print(n*i)
 #   i +=1
 num = [1,3,5,6,7,4,9,10,45,78,89]
+student = ["atif","nabeel","faizi","rs"]
 idx = 0
-while idx < len(num):
-  print(num[idx])
+while idx < len(student):
+  print(student[idx])
   idx +=1
