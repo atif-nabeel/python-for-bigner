0c7b617293a9bc2ab659ac4f3c0710cc6fb39bd1
atif-nabeel <atifnabeel.working@gmail.com>
2026-03-07 11:49:26 +0500

    print list in loop

--- lecture5.py
+++ lecture5.py
@@ -4,8 +4,13 @@
 #   i += 1
 
 # print (" loop ending")
-n = int(input("enter number :"))
-i = 1
-while i <= 10:
-  print(n*i)
-  i +=1
+# n = int(input("enter number :"))
+# i = 1
+# while i <= 10:
+#   print(n*i)
+#   i +=1
+num = [1,3,5,6,7,4,9,10,45,78,89]
+idx = 0
+while idx < len(num):
+  print(num[idx])
+  idx +=1
