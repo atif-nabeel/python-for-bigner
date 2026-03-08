432eb47f8a7c9c3b8bc60124bf60acad3af8187d
atif-nabeel <atifnabeel.working@gmail.com>
2026-03-08 19:06:33 +0900

    loop end

--- lecture5.py
+++ lecture5.py
@@ -9,15 +9,22 @@
 # while i <= 10:
 #   print(n*i)
 #   i +=1
-num =(1,4,5,6,7,8,9,5,8)
-# student = ["atif","nabeel","faizi","rs"]
+# num =(1,4,5,6,7,8,9,5,8)
+# # student = ["atif","nabeel","faizi","rs"]
 
-x = [ 4,6,9]
+# x = [ 4,6,9]
+# i = 0
+
+# while i < len(num):
+#     if num[i] in x:
+#         print("found at idx",num[i] ,i)
+#     else:
+#         print("finding..")
+    # i += 1
 i = 0
-
-while i < len(num):
-    if num[i] in x:
-        print("found at idx",num[i] ,i)
-    else:
-        print("finding..")
-    i += 1
+while i <=5:
+    print(i)
+    if(i == 3):
+        break 
+        i += 1
+        print(" loop end")
