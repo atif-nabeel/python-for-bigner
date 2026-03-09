f20fefde5c00c46b0eecaf922c62eed245022a25
atif-nabeel <atifnabeel.working@gmail.com>
2026-03-09 23:39:09 +0900

    continue

--- lecture5.py
+++ lecture5.py
@@ -28,9 +28,9 @@
 #         break
 #     i += 1
 i = 0
-while i<5:
-    if(i==3):
-        i +=1
+while i < 5:
+    if i == 3:
+        i += 1
         continue
-        print(i)
-        i +=1
+    print(i)
+    i += 1
