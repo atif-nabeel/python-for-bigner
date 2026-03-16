952720f5266ca565b5cff6d9088d2a048df847ce
atif-nabeel <atifnabeel.working@gmail.com>
2026-03-16 15:41:53 +0500

    factorial

--- lecture5.py
+++ lecture5.py
@@ -95,10 +95,15 @@
 # for i in range (1,n+1):
 #     sum +=i
 # print("total sum =",sum)
-n = 8
-sum = 0
-i = 1
-while i <=n:
-    sum +=i
-    i+=1
-    print("total sum =",sum)
+# n = 8
+# sum = 0
+# i = 1
+# while i <=n:
+#     sum +=i
+#     i+=1
+#     print("total sum =",sum)
+n =5
+fact = 1
+for i in range(1,n+1):
+    fact *= i
+    print("factorial =",fact)
