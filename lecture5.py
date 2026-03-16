b1c760958b611975dfc5fdf4d3efbc9729aa7e10
atif-nabeel <atifnabeel.working@gmail.com>
2026-03-16 15:39:10 +0500

    shuy

--- lecture5.py
+++ lecture5.py
@@ -90,8 +90,15 @@
 #     pass
 # print("Faaaaaaaaaaaah")
 
-n = 12
+# n = 12
+# sum = 0
+# for i in range (1,n+1):
+#     sum +=i
+# print("total sum =",sum)
+n = 8
 sum = 0
-for i in range (1,n+1):
+i = 1
+while i <=n:
     sum +=i
-print("total sum =",sum)
+    i+=1
+    print("total sum =",sum)
