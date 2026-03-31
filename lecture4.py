21d220e6b3bc144dc6a51d2018c6d17907ef8959
atif-nabeel <atifnabeel.working@gmail.com>
2026-03-31 21:49:09 +0500

    sgfdt

--- lecture4.py
+++ lecture4.py
@@ -6,46 +6,46 @@
 #   "marks" : [ 98,97,96,95],
 #   "subjects" : [ "python","c","javascript","css"],
 # }
-# print(student)
-# student["Name"] = "Nabeel"
+# # print(student)
+# # student["Name"] = "Nabeel"
 
-# print(student["Name"])
+# # print(student["Name"])
 
-# print(student["age"])
-# print(student["CGPA"])
-# print(student["marks"])
-# print(student["subjects"])
-# null_dict = {}
-# null_dict["name"] = " muhammad Faizan "
-# null_dict["age"] = 20
-
-
+# # print(student["age"])
+# # print(student["CGPA"])
+# # print(student["marks"])
+# # print(student["subjects"])
+# # null_dict = {}
+# # null_dict["name"] = " muhammad Faizan "
+# # null_dict["age"] = 20
 
 
-# print(null_dict)
-Student ={
-  # new_dict = { "city" : "islamabad", " age " : 16}
-  "name" : " Atif nabeel",
-  "age" : 20,
-  "city" : " lahore"
-  "score" : {
-    "comput" : 98,
-    "math" : 98,
-    "phy" : 97,
-  }
-}
-print(Student.keys())
-print(list(Student.keys()))
-print(Student.values())
-print(list(Student.values()))
-print(list(Student.items()))
-print(Student.get("name"))
-print(Student.get("name2"))
-# Student.update({"name":"kashif"})
-new_dict = { "city" : "islamabad", " age " : 16}
-Student.update(new_dict)
-print(Student)
-collection = {1,2,3,4,6,9}
+
+
+# # print(null_dict)
+# Student ={
+#   # new_dict = { "city" : "islamabad", " age " : 16}
+#   "name" : " Atif nabeel",
+#   "age" : 20,
+#   "city" : " lahore"
+#   "score" : {
+#     "comput" : 98,
+#     "math" : 98,
+#     "phy" : 97,
+#   }
+# }
+# print(Student.keys())
+# print(list(Student.keys()))
+# print(Student.values())
+# print(list(Student.values()))
+# print(list(Student.items()))
+# print(Student.get("name"))
+# print(Student.get("name2"))
+# # Student.update({"name":"kashif"})
+# new_dict = { "city" : "islamabad", " age " : 16}
+# Student.update(new_dict)
+
+collection = {1,2,99,77,11,23,12,13,3,4,6,9}
 set2 = {1,5,7,8,10,12}
 print(collection.union(set2))
 print(collection.intersection(set2))
