a1da1291cb4ef0f53e4239bc41ba1a16be6ee16c
atif-nabeel <atifnabeel.working@gmail.com>
2026-04-04 20:12:11 +0500

    sj

--- lecture3.py
+++ lecture3.py
@@ -1,9 +1,9 @@
 marks = [66.7,88.9,56.9,93.5]
 print(marks)
-print(type(marks))
-print(marks[0])
-print (marks[1])
-print(len(marks))
+# print(type(marks))
+# print(marks[0])
+# print (marks[1])
+# print(len(marks))
 # print(" List slicing")
 # marks = [45,66,89,76,65,43]
 # print(marks)
